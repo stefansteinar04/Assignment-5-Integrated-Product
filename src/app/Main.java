934842084ae9d9a0a19d594975590df848502a33
Kristján Jakob Ásgrímsson <kristjanjakobasgrimsson@askja-dw129.rhi.hi.is>
2026-04-19 13:28:50 +0000

    Grunnur út frá model view controller

---
+++ src/app/Main.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        HotelController controller = new HotelController();
+        Scanner sc = new Scanner(System.in);
+        
+    }
+}
---
+++ src/model/Hotel.java
@@ -0,0 +1,12 @@
+public class Hotel {
+    private String name;
+    private String city;
+
+    public Hotel(String name, String city) {
+        this.name = name;
+        this.city = city;
+    }
+
+    public String getName() { return name; }
+    public String getCity() { return city; }
+}
